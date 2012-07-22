41421158e092978686e58066ceed102f34f9ccc4
Mariano Bianchi <marianobianchi08+git@gmail.com>
2012-07-22 15:57:17 -0300

    Armando el esqueleto para el TP

---
+++ agent.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+
+# Si quieren importar algo del otro archivo, pueden hacerlo así
+# from environment import Environment
+
+class Agent(object):
+    def __init__(self, environment):
+        self.environment = environment
+
---
+++ environment.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+
+class Environment(object):
+    def __init__(self):
+        pass
+    
+    def make_action(self, action):
+        reinforce = None
+        next_state = None
+        return (next_state, reinforce)
