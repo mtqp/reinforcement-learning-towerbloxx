7000d4816ec19e39fb0eebdead07118f19456705
mtqp <mtqp@mtqp-desktop.(none)>
2012-08-14 19:52:30 -0300

    agregado manejo multiple uiobjects mejora de abstraccion
    TODO: separar en archivos, lo dejo asi solo por conveniencia ahora

--- codigo/ui/main.py
+++ codigo/ui/main.py
@@ -3,23 +3,30 @@
 import sys, pygame
 #from towerbloxx import * 
 
+HORIZONTAL = "horizontal"
+VERTICAL = "vertical"
+
+
 class UIObject(object):
     def __init__(self, image_path):
         self.surface = pygame.image.load(image_path)
-        self.rect = self.surface.get_rect()
+        self.rectangle = self.surface.get_rect()
         self.name = image_path
-        
-    def name(self):
-        return self.name
-
-    def get_surface(self):
-        return self.surface
-        
-    def get_rectangle(self):
-        return self.rect
-        
-    def move(self, speed):
-        self.rect = self.rect.move(speed)
+        self.horizontal_speed = 0
+        self.vertical_speed = 0
+                
+    def set_speed(self, horizontal, vertical):
+        self.horizontal_speed = horizontal
+        self.vertical_speed = vertical
+    
+    def reverse_speed(self, direction):
+        if direction == HORIZONTAL:
+            self.horizontal_speed = -self.horizontal_speed
+        else:
+            self.vertical_speed = -self.vertical_speed
+    
+    def move(self):
+        self.rectangle = self.rectangle.move([self.horizontal_speed, self.vertical_speed])
     
 
 class UI(object):
@@ -28,39 +35,44 @@ class UI(object):
     def __init__(self, towerbloxx):
         self.width = 640
         self.height = 480
-        self.speed = [2, 2]
         self.size = self.width, self.height
         #self.images = images #ACA VA ALGO O QUE CARAJO?
-        self.ui_objects = [UIObject("ball.bmp")] #esto va a llevar una lista de las cosas q se carguen
+        self.ui_objects = [UIObject("ball.bmp"), UIObject("warning.png")] #esto va a llevar una lista de las cosas q se carguen
         self.screen = pygame.display.set_mode(self.size)
         self.towerbloxx = towerbloxx #aca va a estar la interaccion con lo que efectivamente hay q visualizar
+
+        #esto esta para testear
+        self.ui_objects[0].set_speed(2,2)
+        self.ui_objects[1].set_speed(5,6)
         
     def show(self):
         pygame.init()
 
         while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: sys.exit()
+            self._check_for_closure()
 
             for ui_object in self.ui_objects:
-                ui_object.move(self.speed)
+                ui_object.move()
                 
-                rect = ui_object.get_rectangle()
+                rect = ui_object.rectangle
                                 
                 if rect.left < 0 or rect.right > self.width:
-                    self.speed[0] = -self.speed[0]
+                    ui_object.reverse_speed(HORIZONTAL)
                 if rect.top < 0 or rect.bottom > self.height:
-                    self.speed[1] = -self.speed[1]
+                    ui_object.reverse_speed(VERTICAL)
 
             self._refresh_screen()
             
+    def _check_for_closure(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                sys.exit()
+
         
     def _refresh_screen(self):
         self.screen.fill(UI.BLACK)
         for ui_object in self.ui_objects:
-            surface = ui_object.get_surface()
-            rect = ui_object.get_rectangle()
-            self.screen.blit(surface, rect)
+            self.screen.blit(ui_object.surface, ui_object.rectangle)
         pygame.display.flip()
         
         
