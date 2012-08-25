d72023339375048fb144cec7ab72f6f92dffe3b4
pablo <pablo.brusco@gmail.com>
2012-08-25 14:55:48 -0300

    deleting old file

--- codigo/agent.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-
-from random import random
-
-from environment import Environment
-
-class Agent(object):
-    def __init__(self, environment):
-        self.environment = environment
