6eca1502be11ff39e659937b882e623e3b1dd260
Pablo B <pablo.brusco@gmail.com>
2012-11-23 19:20:17 -0300

    ajustando valores para plotear

--- codigo/pruebaConsola.py
+++ codigo/pruebaConsola.py
@@ -20,10 +20,8 @@ def sAgent(pisos, desde, alpha, gamma, epsilon, lambda_val=0.3):
                        lambda_val=lambda_val)
 
 def progreso_de_refuerzos():
-    comparacion_epsilon(3000)
-    comparacion_gamma(3000)
-    comparacion_alpha(3000)
-    comparacion_qlearning_vs_sarsa(1000)
+    #comparacion_alpha(1000)
+    comparacion_qlearning_vs_sarsa(3)
 
 def run_for(title, n, agents, formatt, labels):
     data = []
@@ -31,26 +29,29 @@ def run_for(title, n, agents, formatt, labels):
     for j in range(len(agents)):
         refs = []
         proms = []
-        for _ in range(n):
+        for i in range(n):
+	    print str(i) + " (Agent: " + labels[j] + ")"	
             refs.append(agents[j].run_episode())
             proms.append(average(refs))
         data.append((refs[:], formatt[j], labels[j]))
-        data.append((proms[:], formatt[j][0] + '-', labels[j] + ' prom'))
+        data.append((proms[:], formatt[j][0] + '-','Promedio'))
         
 
     plot_all(title,data)
     
 def comparacion_epsilon(n):
-    qagent1 = qAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0.0001)
-    qagent2 = qAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0.001)
-    qagent3 = qAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0.01)
+
+    qagent1 = qAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0)
+    qagent2 = qAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0.0001)
+    qagent3 = qAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0.001)
+    qagent4 = qAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0.01)
     
     run_for(
         u"Comparación Epsilons en QLearning",
         n,
-        [qagent1,qagent2,qagent3],
-        ["g.","r.","b."],
-        ["QAgent eps=0.0001","QAgent eps=0.001","QAgent eps=0.01"]
+        [qagent1,qagent2,qagent3,qagent4],
+        ["y.","m.","c.","r."],
+        ["Q(e=0)", "Q(e=0.0001)","Q(e=0.001)","Q(e=0.01)"]
     )
 
 def comparacion_gamma(n):
@@ -67,6 +68,7 @@ def comparacion_gamma(n):
     )
     
 def comparacion_alpha(n):
+    qagent0 = qAgent(pisos=20, desde=20, alpha=0, gamma=0.7, epsilon=0.001)
     qagent1 = qAgent(pisos=20, desde=20, alpha=0.2, gamma=0.7, epsilon=0.001)
     qagent2 = qAgent(pisos=20, desde=20, alpha=0.5, gamma=0.7, epsilon=0.001)
     qagent3 = qAgent(pisos=20, desde=20, alpha=0.8, gamma=0.7, epsilon=0.001)
@@ -74,22 +76,22 @@ def comparacion_alpha(n):
     run_for(
         u"Comparación Alphas en QLearning",
         n,
-        [qagent1,qagent2,qagent3],
-        ["g.","r.","b."],
-        ["QAgent alpha=0.2","QAgent alpha=0.5","QAgent alpha=0.8"]
+        [qagent0, qagent1,qagent2,qagent3],
+        ["m.","c.","r.","b."],
+        ["","QAgent alpha=0.2","QAgent alpha=0.5","QAgent alpha=0.8"]
     )
 def comparacion_qlearning_vs_sarsa(n):
-    #qAgent:
-    # epsilon: 0.0001, 0.001, 0.01
     qagent = qAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0.001)
-    sarsa = sAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, epsilon=0.001)
+    sarsa = sAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, lambda_val = 0.001, epsilon=0.001)
+    sarsa2 = sAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, lambda_val = 0.3, epsilon=0.001)
+    sarsa3 = sAgent(pisos=20, desde=20, alpha=0.7, gamma=0.8, lambda_val = 0.7, epsilon=0.001)
     
     run_for(
-        u"Comparación QLearning vs Sarsa",
+        u"Comparación QLearning vs Sarsas",
         n,
-        [qagent,sarsa],
-        ["g.","r."],
-        ["QLearning","SarsaLambda"]
+        [qagent,sarsa,sarsa2,sarsa3],
+        ["c.","m.", "r.","b."],
+        ["QLearning","Sarsa (lamda=0.001)","Sarsa (lamda=0.03)","Sarsa (lambda=0.07)"]
     )
 progreso_de_refuerzos()
 
--- codigo/utils.py
+++ codigo/utils.py
@@ -2,29 +2,6 @@
 
 import matplotlib.pyplot as plt
 
-
-"""
-Cómo graficar: (sacado de http://matplotlib.org/users/pyplot_tutorial.html)
-
-1) Grafico simple:
-    - plt.plot(y_values)
-    - plt.plot(xvalues, yvalues)  # Debe valer que: len(xvalues)==len(yvalues)
-
-2) Muchos graficos en una figura:
-    - Subplots:
-        plt.figure(1)
-        # El primer numero es la cantidad de filas, el segundo es la cant de
-        # columnas y el tercero el numero del subgrafico, contando de izquierda
-        # a derecha y de arriba hacia abajo
-        plt.subplot(211)
-        plt.plot(x, y)
-        plt.subplot(212)
-        plt.plot(x2, y2)
-    - Varias funciones en el mismo grafico:
-        plt.plot(x1, y1, x2, y2, x3, y3, ...)
-
-"""
-
 def plot_all(title, graphs):
     plots = []
     legends = []
@@ -36,26 +13,8 @@ def plot_all(title, graphs):
     plt.title(title)
     plt.xlabel("Episodios")
     plt.ylabel("Refuerzo")
-    plt.legend(plots, legends, loc = 2) #4 es abajo a la derecha
-    
-    leg = plt.gca().get_legend()
-    ltext  = leg.get_texts()  # all the text.Text instance in the legend
-
-    plt.setp(ltext, fontsize=8)    # the legend text fontsize
+    plt.legend(plots, legends, loc = 4,prop={'size':6}) #4 es abajo a la derecha
+   
     plt.savefig(title+".svg")
     
     
-
-def progreso_refuerzos_subplots(refs_q, refs_s):
-    plt.subplot(211)
-    plt.plot(refs_q)
-    plt.title("Progreso de refuerzos para Q Learning")
-    plt.xlabel("Nro de episodio")
-    plt.ylabel("Suma de los refuerzos por episodio")
-    plt.subplot(212)
-    plt.plot(refs_s)
-    plt.title("Progreso de refuerzos para Sarsa lambda")
-    plt.xlabel("Nro de episodio")
-    plt.ylabel("Suma de los refuerzos por episodio")
-    plt.savefig("progreso_refuerzos_subplots.jpg")
-
